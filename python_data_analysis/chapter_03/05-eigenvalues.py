d965e2f58ad19708db03128adf0fe8a301f0330a
Fernando Chimicoviaki <fernando.chimicoviaki@gmail.com>
2016-06-28 21:49:11 -0300

    create new files

--- python_data_analysis/chapter_03/05-eigenvalues.py
+++ python_data_analysis/chapter_03/05-eigenvalues.py
@@ -7,7 +7,7 @@ print "A\n", A
 
 print "Eigenvalues", np.linalg.eigvals(A)
 
-eigenvalues, eigenvectors = np.linealg.eig(A)
+eigenvalues, eigenvectors = np.linalg.eig(A)
 print "First tuple of eig", eigenvalues
 print "Second tuple of eig\n", eigenvectors
 

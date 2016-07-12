767e161581a8581c4a96dc4cb3cddcd398e2f3ff
Fernando Chimicoviaki <fernando.chimicoviaki@gmail.com>
2016-07-11 21:58:52 -0300

    update file 09

--- python_data_analysis/chapter_04/09-pivot_demo.py
+++ python_data_analysis/chapter_04/09-pivot_demo.py
@@ -1,17 +1,25 @@
 # coding: utf-8
 import pandas as pd
 import numpy as np
+from numpy.random import seed
+from numpy.random import rand
+from numpy.random import random_integers
 
+seed(42)
+N = 7
 
-df = pd.read_csv(
-    "/home/fernando/Projects/python-data-analysis/" +
-    "python_data_analysis/chapter_04/WHO_first9cols.csv")
-df = df[['Country', df.columns[-2]]][:2]
-print "New df\n", df
-
-print "Null values\n", pd.isnull(df)
-print "Total Null Values\n", pd.isnull(df).sum()
-print "Not Null Values\n", pd.notnull(df)
-print "Last Column Doubled\n", 2 * df[df.columns[-1]]
-print "Last Column Plus NaN\n", df[df.columns[-1]] + np.nan
-print "Zero filled\n", df.fillna(0)
+df = pd.DataFrame(
+    {
+        "Weather": [
+            "cold", "hot", "cold", "hot",
+            "cold", "hot", "cold"
+        ],
+        "Food": [
+            "soup", "soup", "icecream", "chocolate",
+            "icecream", "icecream", "soup"
+        ],
+        "Price": 10 * rand(7),
+        "Number": random_integers(1, 9, size=(7, ))
+    })
+print "DataFrame\n", df
+print pd.pivot_table(df, columns=["Food"], aggfunc=np.sum)
